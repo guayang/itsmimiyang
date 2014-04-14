9c12e20f7c68dbc944fd510995515742fa51602c
guayang <guanya.yang@gmail.com>
2014-04-14 14:32:03 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Itsmimiyang::Application.config.secret_key_base = 'dfc73dfed76ced1be4218bec540b0248da61c205621fe6b90169ab1fea4f6158a385b45d1a7e7ae93c047787fc6973825321d2a6938f9951afb30657c7c4c2b3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Itsmimiyang::Application.config.session_store :cookie_store, key: '_itsmimiyang_session'
